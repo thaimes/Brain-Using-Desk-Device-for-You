4daff0e1c11ef6283b2ad272f683b361551521dd
linn87rinn <im.linhnguyen.17@gmail.com>
2025-10-24 16:26:29 -0500

    Enhance ESP_Reading
    Refactor ESP_Reading.ino to improve motor control logic and LED indication based on camera input.

--- TestFiles/ESP_Reading.ino
+++ TestFiles/ESP_Reading.ino
@@ -1,21 +1,42 @@
-#define LED_PIN 2  // built-in LED on most ESP32 Dev boards
+#define LED_PIN 2
+#include<motor.h>
 
+bool trashWasPresent = false;
 void setup() {
-
-  Serial.begin(115200);  // USB to computer
-  Serial2.begin(115200, SERIAL_8N1, 16, 17); // RX2=16, TX2=17
-  Serial.println("DEV READY: listening for CAM...");
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  setupMotor();
+  Serial.begin(115200);  // USB to PC (uses GPIO1/3 internally)
+  Serial2.begin(115200, SERIAL_8N1, 16, 17); // <-- RX2=16, TX2=17
+  Serial.println("DEV READY: Listening...");
 }
-
 void loop() {
- if (Serial2.available()) {
-    String msg = Serial2.readStringUntil('\n');
-    msg.trim();
-    Serial.print("FROM CAM X: ");
-    Serial.println(msg);
-    
-    int x = msg.toInt();   // convert to integer
-    // you can now use x position for control logic
-}
-}
+    if (Serial2.available()) {
+        String msg = Serial2.readStringUntil('\n');
+        msg.trim();
+        Serial.print("FROM CAM: "); 
+        Serial.println(msg);
+        if (msg.startsWith("{\"trash\":")) {
+            digitalWrite(LED_PIN, HIGH);
+            Serial.println("TRASH APPEARED");
+            spinServoForward();
 
+            int start = msg.indexOf(":") + 1;
+            int end = msg.indexOf("}");
+            int x = msg.substring(start, end).toInt();
+
+            if (x <= 24) {
+                Serial.println("STOP MOTOR");
+                stopMotors();
+            } else {
+                Serial.println("START MOTOR");
+                moveForward();
+            }
+        }
+
+        else{
+            digitalWrite(LED_PIN, LOW);
+            rotateMotors();
+        }
+    }
+}
